c405c8668500cfec9144008e02958b4b6339aa28
wc <1195741928@qq.com>
2019-04-28 18:55:58 +0800

    v1 版

---
+++ library/booklibrary/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'booklibrary'
+
+
+urlpatterns = [
+    url(r'^$',views.index,name='index'),
+    url(r'login/$',views.login,name='login'),
+    url(r'register/$',views.register,name='register'),
+    url(r'loginhandler/$',views.loginhandler,name='loginhandler'),
+    url(r'registerhandler/$',views.registerhandler,name='registerhandler'),
+    url(r'checkstuinfo/(.*?)/$',views.checkstuinfo,name='checkstuinfo'),
+    url(r'updatestuinfo/(.*?)/$',views.updatestuinfo,name='updatestuinfo'),
+    url(r'checkbook/(.*?)/$',views.checkbook,name='checkbook'),
+    url(r'bookdetail/(.*?)/(.*?)/$',views.bookdetail,name='bookdetail'),
+    # url(r'borrow/(.*?)/(.*?)/$',views.borrow,name='borrow'),
+    url(r'show_borrows/$',views.show_borrows,name='show_borrows'),
+
+]
